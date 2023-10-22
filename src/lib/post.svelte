40fb87cc2c907cd5c3e2d5b15ea680f9be333494
Muhammad Zeeshan <77879378+zexhan17@users.noreply.github.com>
2023-10-22 19:03:26 +0500

    Init

---
+++ src/lib/post.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { projects } from "$lib/projects";
+    import { onMount } from "svelte";
+    import { fade, fly } from "svelte/transition";
+    let show = false;
+    onMount(() => {
+        setTimeout(() => {
+            show = true;
+        }, 1000);
+    });
+</script>
+
+{#if show}
+    <p transition:fade={{ duration: 2000 }} class="px-5">
+        Each project in my portfolio represents not just lines of code, but a
+        vision brought to life, a challenge met, and a solution elegantly
+        crafted. From seamless user interfaces to robust backend architectures,
+        every endeavor is a testament to my passion for creating intuitive,
+        delightful experiences. Dive into my projects, and you'll find more than
+        just codes and designs – you'll discover the heart and soul I pour into
+        each line and pixel.
+    </p>
+{/if}
+
+{#each projects as p, i}
+    {#if show}
+        <a target="_blank" href={p.link}>
+            <div
+                target="_blank"
+                href={p.link}
+                transition:fade={{ duration: 2000 }}
+                class="my-5 group hover:bg-slate-800/50 rounded-lg p-5 cursor-pointer hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg"
+            >
+                <h1 class="text-xl font-semibold group-hover:text-teal-300">
+                    {p.title}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        class="inline-block h-4 w-4 shrink-0 transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 ml-1 translate-y-px"
+                        aria-hidden="true"
+                        ><path
+                            fill-rule="evenodd"
+                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                            clip-rule="evenodd"
+                        /></svg
+                    >
+                </h1>
+                <p class="mt-1 mb-3">{p.desc}</p>
+                <div class="gap-3 flex flex-wrap">
+                    {#each p.tags as tag}
+                        <span
+                            class="capitalize flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
+                            >{tag}</span
+                        >
+                    {/each}
+                </div>
+            </div>
+        </a>
+    {/if}
+{/each}
+
+<style>
+    p {
+        color: rgb(148 163 184/1);
+    }
+</style>
---
+++ src/lib/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        title: 'Crowdfunding Web App',
+        link: 'https://grow-hub-zexhan17.vercel.app/public/funded',
+        desc: 'A campaigner can post and donors can support the campaigner. Admin can manage all users, transactions, campaigns.',
+        tags: ['Reactjs', 'tailwind css', 'ASP.NET Core', 'Sql Server']
+    },
+    {
+        title: 'Online Notice Board Web App',
+        link: 'https://notice-board-zexhan17.vercel.app/auth/login',
+        desc: `Admin can publish notice for one specific class or whole department or for the whole college. Admin can manage users. Students can view notices, profiles and can message admin.`,
+        tags: ['sveltekit', 'svelte', 'pocketbase', 'tailwind css', 'daisyUi']
+    }
+]
