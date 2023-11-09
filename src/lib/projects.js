cb9e33b4f50843542bf5423748ff18a693dfeee0
Zeeshan <77879378+zexhan17@users.noreply.github.com>
2023-11-09 21:22:18 +0500

    Update projects.js

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,8 +1,14 @@
 export const projects = [
     {
+        title: 'Expense Tracker',
+        link: 'https://expense-tracker-zexhan17.vercel.app/',
+        desc: `Expense Tracker web application using browser storage not any cloud. No need to install mobile apps for your simple daily tasks.`,
+        tags: ['svelte', 'tailwind css', 'daisyUi']
+    },
+    {
         title: 'Crowdfunding Web App',
         link: 'https://grow-hub-zexhan17.vercel.app/public/funded',
-        desc: 'A campaigner can post and donors can support the campaigner. Admin can manage all users, transactions, campaigns.',
+        desc: 'A Crowdfunding web application where compaigners and donors can help each other. Admin side can handle all users.',
         tags: ['Reactjs', 'tailwind css', 'ASP.NET Core', 'Sql Server']
     },
     {
@@ -10,5 +16,5 @@ export const projects = [
         link: 'https://notice-board-zexhan17.vercel.app/auth/login',
         desc: `Admin can publish notice for one specific class or whole department or for the whole college. Admin can manage users. Students can view notices, profiles and can message admin.`,
         tags: ['sveltekit', 'svelte', 'pocketbase', 'tailwind css', 'daisyUi']
-    }
+    },
 ]
